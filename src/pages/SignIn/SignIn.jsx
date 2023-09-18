2ce2de46bd41c8ee87e44e476fa3ba9572e4c63a
Manish <manish.info2020@gmail.com>
2023-09-18 16:50:11 +0530

    initial setup + added login/signup page

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./SignIn.css";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { BsDiscord } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { AiOutlineApple } from "react-icons/ai";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const SignIn = () => {
+  const { loginWithRedirect } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  console.log(user);
+  return (
+    <>
+      <div className="main-login-container">
+        <div className="social-img left-side">
+          <div className="header">
+            <div className="logo">LOGO</div>
+          </div>
+          <div className="app-name">
+            <div className="board">Board.</div>
+          </div>
+          <div className="social-links">
+            <div className="links">
+              <a href="#" className="icon">
+                <AiOutlineGithub />
+              </a>
+              <a href="#" className="icon">
+                <FaLinkedin />
+              </a>
+              <a href="#" className="icon">
+                <BsDiscord />
+              </a>
+              <a href="#" className="icon">
+                <AiFillTwitterCircle />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="login-container">
+          <div className="login-card">
+            <div className="login-header">
+              <h2>Sign In</h2>
+              <strong>Sign in to your account</strong>
+            </div>
+            <div className="redirect">
+              <div onClick={() => loginWithRedirect()} className="g-a">
+                {" "}
+                <span className="align-c">
+                  <FcGoogle />
+                </span>{" "}
+                <span> Sign in with Google</span>
+              </div>
+              <div className="g-a">
+                <span className="align-c">
+                  <AiOutlineApple />
+                </span>{" "}
+                <span> Sign in with Apple</span>
+              </div>
+            </div>
+
+            <div className="login-items">
+              <button
+                className="login-button"
+                onClick={() => loginWithRedirect()}
+              >
+                Log In
+              </button>
+
+              <div className="login-footer">
+                <p>Don't have an account ? </p>
+
+                <button
+                  className="login-button"
+                  onClick={() => loginWithRedirect()}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
